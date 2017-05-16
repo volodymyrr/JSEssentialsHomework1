210cc4c041d64c0ebf2347156858fbf35e643f67
Volodymyr <danyliak.volodymyrr@gmail.com>
2017-05-16 18:08:55 +0300

    Added files

---
+++ script.js
@@ -0,0 +1,6 @@
+var person_name = prompt("Please enter your name: ");
+var event = prompt("Please enter an event: ");
+var place = prompt("Please enter place: ");
+var time = " today at 16:00";
+var sentence_with_all_values = (person_name+" has a "+event+" in "+place+time+"!")
+console.log(sentence_with_all_values)
